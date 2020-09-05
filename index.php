a9029bd99d4dadb549bf8b74da55826d9678128e
Rômulo Alves <romuloalves.rj@gmail.com>
2020-09-05 09:27:45 -0300

    Code Igniter 4.0.4

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-try{
-    $conn = new PDO('pgsql:host=localhost;port=5432;dbname=rswagenda','postgres','root');
-    if ($conn) {
-        echo "database conectado";
-    }
-} catch (PDOException $e) {
-    echo $e->getMessage();
-} 
